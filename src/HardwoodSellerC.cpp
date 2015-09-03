568c0f57dd1275ff601ffb8d9011e7c69c4cb3b2
Omar4444 <umara4@gmail.com>
2015-09-03 02:49:07 -0400

    Bugs fixed
    Fixed some issues with strtok() and final results layout.

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	wi.WoodItem();
+	WoodItem();
 	readInputFile(argv[1]);
 	printOrder();
 
@@ -41,29 +41,35 @@ void readInputFile(string inputFilePath) {
 	// read the first line of the file
 	fs.getline(buf, bufSize);
 	// print out the name, address, and date
-	cout << strtok(buf, ";") << endl << strtok(buf, ";") << endl << strtok(buf, " ") << endl;
+	tokT = strtok(buf, ";");
+	cout << tokT << endl;
+	tokT = strtok(NULL, ";");
+	cout << tokT << endl;
+	tokT = strtok(NULL, ";");
+	cout << tokT << endl;
+
 	// read the 2nd line of the file
 	fs.getline(buf, bufSize);
 	// read the order
 	tokT = strtok(buf, ":");
 	while (tokT != NULL)
 	{
-		tokN = strtok(buf, ";");
-		if (strncmp("Cherry", tokT, sizeof("Cherry")))
+		tokN = strtok(NULL, ";");
+		if (strncmp("Cherry", tokT, sizeof("Cherry")) == 0)
 			wi.cherry = atoi(tokN);
-		else if (strncmp("Curly Maple", tokT, sizeof("Curly Maple")))
+		else if (strncmp("Curly Maple", tokT, sizeof("Curly Maple")) == 0)
 			wi.cMaple = atoi(tokN);
-		else if (strncmp("Genuine Mahogany", tokT, sizeof("Genuine Mahogany")))
+		else if (strncmp("Genuine Mahogany", tokT, sizeof("Genuine Mahogany")) == 0)
 			wi.gMaho = atoi(tokN);
-		else if (strncmp("Wengey", tokT, sizeof("Wengey")))
+		else if (strncmp("Wenge", tokT, sizeof("Wenge")) == 0)
 			wi.wenge = atoi(tokN);
-		else if (strncmp("White Oak", tokT, sizeof("White Oak")))
+		else if (strncmp("White Oak", tokT, sizeof("White Oak")) == 0)
 			wi.wOak = atoi(tokN);
-		else if (strncmp("Sawdust", tokT, sizeof("Sawdust")))
+		else if (strncmp("Sawdust", tokT, sizeof("Sawdust")) == 0)
 			wi.sawdust = atoi(tokN);
 		else
 			cout << tokT << " is unknown." << endl;
-		tokT = strtok(buf, ":");
+		tokT = strtok(NULL, ":");
 	}
 }
 
@@ -96,20 +102,20 @@ double deliveryTime() {
 * Print the list
 */
 void printOrder(){
-	cout << "\n Ordered wood\t BF purchased\t price \n\n";
+	cout << "\n Ordered wood\t\t BF purchased\t  price \n\n";
 	if (wi.cherry != 0)
-		cout << "Cherry\t   " << wi.cherry << "\t   $" << wi.cherry * 5.95 << endl;
+		cout << "  Cherry\t\t   " << wi.cherry << "\t\t   $" << wi.cherry * 5.95 << endl;
 	if (wi.cMaple != 0)
-		cout << "Curly Maple\t   " << wi.cMaple << "\t   $" << wi.cMaple * 6 << endl;
+		cout << "  Curly Maple\t\t   " << wi.cMaple << "\t\t   $" << wi.cMaple * 6 << endl;
 	if (wi.gMaho != 0)
-		cout << "Genuine Mahogany\t   " << wi.gMaho << "\t   $" << wi.gMaho * 9.6 << endl;
+		cout << "  Genuine Mahogany\t   " << wi.gMaho << "\t\t   $" << wi.gMaho * 9.6 << endl;
 	if (wi.wenge != 0)
-		cout << "Wengey\t   " << wi.wenge << "\t   $" << wi.wenge * 22.35 << endl;
+		cout << "  Wenge\t\t\t   " << wi.wenge << "\t\t   $" << wi.wenge * 22.35 << endl;
 	if (wi.wOak != 0)
-		cout << "White Oak\t   " << wi.wOak << "\t   $" << wi.wOak * 6.7 << endl;
+		cout << "  White Oak\t\t   " << wi.wOak << "\t\t   $" << wi.wOak * 6.7 << endl;
 	if (wi.sawdust != 0)
-		cout << "Sawdust\t   " << wi.sawdust << "\t   $" << wi.sawdust * 1.5 << endl;
+		cout << "  Sawdust\t\t   " << wi.sawdust << "\t\t   $" << wi.sawdust * 1.5 << endl;
 
 	cout << "The estimated delivery time is: " << deliveryTime() << endl;
-	cout << "The total price is: " << wi.cherry * 5.95 + wi.cMaple * 6 + wi.gMaho * 9.6 + wi.wenge * 22.35 + wi.wOak * 6.7 + wi.sawdust * 1.5 << endl;
+	cout << "The total price is: $" << wi.cherry * 5.95 + wi.cMaple * 6 + wi.gMaho * 9.6 + wi.wenge * 22.35 + wi.wOak * 6.7 + wi.sawdust * 1.5 << endl;
 }
